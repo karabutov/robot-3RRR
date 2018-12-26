a34ea23864c79850a0bfd453534ff9390a4a0db4
Mikhail Posypkin <mposypkin@gmail.com>
2018-12-26 14:20:57 +0300

    fix middle

--- areas/area.py
+++ areas/area.py
@@ -7,7 +7,7 @@ import matplotlib.patches as ps
 import matplotlib.lines as ls
 import matplotlib.path as ph
 
-EPSILON = 0.04
+EPSILON = 0.1
 
 size_side = 1.
 k = (2./3.) * (size_side * math.sqrt(3.) / 2.)
@@ -109,45 +109,21 @@ def third_equation(args):
 	return (x + k * math.cos(gamma3 + phi) - xB3)**2 + (y + k * math.sin(gamma3 + phi) - yB3)**2 - l**2
 
 def minus_first_equation(args):
-	x =	args[0]
-	y =	args[1] 
-	phi = args[2]
-	theta1 = args[3]
-	theta2 = args[4]
-	theta3 = args[5]
-	xB1 = x1 + l * math.cos(theta1)
-	yB1 = y1 + l * math.sin(theta1)
-	return -(x + k * math.cos(gamma1 + phi) - xB1)**2 - (y + k * math.sin(gamma1 + phi) - yB1)**2 + l**2
+	return -first_equation(args)
 
 def minus_second_equation(args):
-	x =	args[0]
-	y =	args[1] 
-	phi = args[2]
-	theta1 = args[3]
-	theta2 = args[4]
-	theta3 = args[5]
-	xB2 = x2 + l * math.cos(theta2)
-	yB2 = y2 + l * math.sin(theta2)
-	return -(x + k * math.cos(gamma2 + phi) - xB2)**2 - (y + k * math.sin(gamma2 + phi) - yB2)**2 + l**2
+	return -second_equation(args)
 
 def minus_third_equation(args):
-	x =	args[0]
-	y =	args[1] 
-	phi = args[2]
-	theta1 = args[3]
-	theta2 = args[4]
-	theta3 = args[5]
-	xB3 = x3 + l * math.cos(theta3)
-	yB3 = y3 + l * math.sin(theta3)
-	return -(x + k * math.cos(gamma3 + phi) - xB3)**2 - (y + k * math.sin(gamma3 + phi) - yB3)**2 + l**2
+	return -third_equation(args)
 
 def there_is_a_solution(aria):
 
 	args_b = np.array([aria[0][0], aria[1][0], aria[2][0], aria[3][0], aria[4][0], aria[5][0]])
 	args_e = np.array([aria[0][1], aria[1][1], aria[2][1], aria[3][1], aria[4][1], aria[5][1]])
 
-	min_solut = so.minimize(first_equation, (args_e - args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
-	max_solut = so.minimize(minus_first_equation, (args_e - args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
+	min_solut = so.minimize(first_equation, (args_e + args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
+	max_solut = so.minimize(minus_first_equation, (args_e + args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
 	
 	max_of_f = -max_solut.fun
 	min_of_f = min_solut.fun
@@ -155,8 +131,8 @@ def there_is_a_solution(aria):
 	if not(max_of_f >= 0. and min_of_f <= 0.):
 		return False
 
-	min_solut = so.minimize(second_equation, (args_e - args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
-	max_solut = so.minimize(minus_second_equation, (args_e - args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
+	min_solut = so.minimize(second_equation, (args_e + args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
+	max_solut = so.minimize(minus_second_equation, (args_e + args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
 
 	max_of_f = -max_solut.fun
 	min_of_f = min_solut.fun
@@ -164,8 +140,8 @@ def there_is_a_solution(aria):
 	if not(max_of_f >= 0. and min_of_f <= 0.):
 		return False
 
-	min_solut = so.minimize(third_equation, (args_e - args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
-	max_solut = so.minimize(minus_third_equation, (args_e - args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
+	min_solut = so.minimize(third_equation, (args_e + args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
+	max_solut = so.minimize(minus_third_equation, (args_e + args_b) / 2., bounds = ((args_b[0], args_e[0]), (args_b[1], args_e[1]), (args_b[2], args_e[2]), (args_b[3], args_e[3]), (args_b[4], args_e[4]), (args_b[5], args_e[5])))
 
 	max_of_f = -max_solut.fun
 	min_of_f = min_solut.fun
